297f48364bf3456d21def50038bee4192fba8fea
DESKTOP-QDRF8S5\Настя <bi-bi-bu@mail.ru>
2017-04-05 17:56:16 +0300

    day work

--- XMLParser/parser.js
+++ XMLParser/parser.js
@@ -2,28 +2,37 @@ var xmlData = {},
     xmlString = prompt("Please, alert some XML-string:", '<book title="Book 1"><chapter title="Chapter 1"><paragraph>123</paragraph><paragraph>456</paragraph></chapter></book>>'),
     xmlStringArray = xmlString.split('');
 
-function parse(array){
+function readTag(array){
     for (var i = 0; i < array.length; i++) {
-        var currentNumber = 0;
         if (array[i] === "<") {
             var objNameArr = [],
-                objName = "";
-            for (var j = 1; j < array.length; j++) {
-                if (array[j] !== ">" && array[j] !== " ") {
-                    objNameArr.push(array[j])
-                }
-                else {
-                    currentNumber = j;
-                    objName = objNameArr.join("");
-                    xmlData[objName] = {};
-                    break;
+                objAttributeArr = [];
+            findtag: for (var j = 1; j < array.length; j++) {
+                switch(array[j]){
+                    case ">":
+                        xmlData[objNameArr.join("")] = {};
+                        if(objAttributeArr > 0){
+                            
+                        }
+                        break;
+                        break findtag;
+
+                    case " ":
+                        objAttributeArr.push(array[j]);
+                        break;
+
+                    case "=":
+                        xmlData[objNameArr.join("")][objAttributeArr.join("")] = {};
+                        break;
+
+                    default: objNameArr.push(array[j]);
                 }
             }
         }
     }
 }
 
-parse(xmlString);
+readTag(xmlString);
 console.log(xmlData);
 
 // xmlParser.stringify = function(xmlString){};
